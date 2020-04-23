2a9eaea2a7028e0e216766686e4cf57fd61c5fc6
unknown <diegofabiano1891@gmail.com>
2020-04-23 20:59:38 -0300

    'versionFinal'

---
+++ Recursos/js/controlador/controlador.js
@@ -0,0 +1,32 @@
+/*
+ * Controlador
+ */
+var Controlador = function(modelo) {
+  this.modelo = modelo;
+};
+
+Controlador.prototype = {
+  agregarPregunta: function(pregunta, respuestas) {
+    this.modelo.agregarPregunta(pregunta, respuestas);
+  },
+
+
+  borrarPregunta: function(id) {
+    this.modelo.borrarPregunta(id);
+  }, 
+
+
+  editarPregunta: function(id,nuevaPregunta) {
+    this.modelo.editarPregunta(id,nuevaPregunta);
+  }, 
+
+
+  borrarTodasLasPreguntas: function() {
+    this.modelo.borrarTodasLasPreguntas();
+  },  
+
+  
+  agregarVoto: function(nombrePregunta,respuestaSeleccionada) {
+    this.modelo.agregarVoto(nombrePregunta,respuestaSeleccionada);
+  },  
+};
---
+++ Recursos/js/modelo/modelo.js
@@ -0,0 +1,98 @@
+/* Modelo  */
+
+var Modelo = function() {
+  var datosEnLocalStorage = JSON.parse(localStorage.getItem('encuestas'));
+  if (datosEnLocalStorage == null) {
+    this.preguntas = [];
+  } else {
+    this.preguntas = datosEnLocalStorage;
+  }
+  
+  this.ultimoId = 0;
+
+  //inicializacion de eventos
+  this.preguntaAgregada = new Evento(this);
+  this.preguntaEliminada = new Evento(this);
+  this.preguntaEditada = new Evento(this); 
+  this.todoBorrado = new Evento(this);  
+  this.votoGuardado = new Evento(this); 
+};
+
+Modelo.prototype = {
+  //se obtiene el id más grande asignado a una pregunta
+  obtenerUltimoId: function() {
+    var cantPreguntas = this.preguntas.length;
+    var ultimoId;
+    if (cantPreguntas == 0) {
+      ultimoId = 0;
+    } else {
+      ultimoId = this.preguntas[cantPreguntas-1].id
+    }
+    return ultimoId;
+  },
+
+  //se agrega una pregunta dado un nombre y sus respuestas
+  agregarPregunta: function(nombre, respuestas) {
+    var id = this.obtenerUltimoId();
+    id++;
+    var nuevaPregunta = {'textoPregunta': nombre, 'id': id, 'cantidadPorRespuesta': respuestas};
+    this.preguntas.push(nuevaPregunta);
+    this.guardar();
+    this.preguntaAgregada.notificar();
+  },
+
+  // metodo agregado. Pampa.-
+  borrarPregunta: function(id){
+    this.preguntas = this.preguntas.filter(pregunta => pregunta.id !== id);
+    this.guardar();  
+    this.preguntaEliminada.notificar();
+  },
+
+
+  // metodo agregado. Pampa.-
+  editarPregunta: function(id,nuevaPregunta){
+    var indiceElementoAEditar;
+    this.preguntas.some(function(entry, i) {
+      if (entry.id == id) {
+        indiceElementoAEditar = i;
+       }
+    });
+  
+    this.preguntas[indiceElementoAEditar].textoPregunta = nuevaPregunta;
+    this.guardar(); 
+    this.preguntaEditada.notificar();
+    },
+
+  // metodo agregado. Pampa.-
+  borrarTodasLasPreguntas: function() { 
+    this.preguntas = [];
+    this.guardar();  
+    this.todoBorrado.notificar();
+  },  
+
+  agregarVoto: function(nombrePregunta,respuestaSeleccionada) { 
+    var indicePregunta;
+    this.preguntas.some(function(posicion, i) {
+      if (posicion.textoPregunta === nombrePregunta) {
+        indicePregunta = i;
+       }
+    });
+
+    let posiblesRespuestas = this.preguntas[indicePregunta].cantidadPorRespuesta;
+
+    var indiceRespuestaVotada = posiblesRespuestas.findIndex(respuestas => respuestas.textoRespuesta == respuestaSeleccionada)
+    let votos = this.preguntas[indicePregunta].cantidadPorRespuesta[indiceRespuestaVotada].cantidad;
+
+    votos++;
+    this.preguntas[indicePregunta].cantidadPorRespuesta[indiceRespuestaVotada].cantidad=votos;
+
+    this.guardar();  
+    this.votoGuardado.notificar();
+  },  
+
+  //se guardan las preguntas
+  guardar: function(){
+    localStorage.setItem('encuestas', JSON.stringify(this.preguntas));
+  },
+
+};
